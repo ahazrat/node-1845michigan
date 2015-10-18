2b9ac4e22c35598a443d8653e66ee41cf7a08688
frastr <fractalstrategies@gmail.com>
2015-10-18 18:31:26 +0000

    project rebuild initial commit

---
+++ app/routes.js
@@ -0,0 +1,39 @@
+var Todo = require('./models/todo');
+
+module.exports = function (app) {
+  
+  // api
+  // get all todos
+  app.get('/api/todos', function (req, res) {
+    Todo.find(function (err, todos) {
+      if (err) { res.send(err); }
+      res.json(todos);
+    });
+  });
+  // create todo and send back all todos
+  app.post('/api/todos', function (req, res) {
+    Todo.create({
+      text: req.body.text,
+      done: false
+    }, function (err, todo) {
+      if (err) { res.send(err); }
+      res.redirect('/api/todos');
+    });
+  });
+  // delete a todo
+  app.delete('/api/todos/:todo_id', function (req, res) {
+    Todo.remove({
+      _id: req.params.todo_id
+    }, function (err, todo) {
+      if (err) { res.send(err); }
+      res.redirect('/api/todos');
+    });
+  });
+  
+  // application
+  // get index
+  app.get('*', function (req, res) {
+    res.sendfile('./public/index.html');
+  });
+  
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+// set up
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var port = process.env.PORT || 8080;
+var db = require('./config/database');
+var morgan = require('morgan');  // log requests to the console (express4)
+var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+// config
+mongoose.connect(db.url);
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));  // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());  // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));  // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// routes
+require('./app/routes')(app);
+
+// listen
+app.listen(port);
+console.log('App listening on port ' + port);
