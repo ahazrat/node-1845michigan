41cd25fd3539f9ef348674f993cb4899f822bc21
ahazrat <fractalstrategies@gmail.com>
2015-10-23 16:51:21 +0000

    add favicon, update title

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-
 app.use(bodyParser.json());  // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));  // parse application/vnd.api+json as json
 app.use(methodOverride());
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 // authentication
 app.use(expressSession({secret: 'Grumpy Cat'}));
